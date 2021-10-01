a18f1f624d2d848938d6e3631d829801e6fe9a55
Harsh <trivediharsh49@gmail.com>
2021-10-01 13:49:10 +0530

    added java Spring boot project of Statistics Api

---
+++ RegressionApi/src/test/java/com/harsh/statistics/StatistcsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harsh.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatistcsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
